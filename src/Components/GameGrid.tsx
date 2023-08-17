4a39cc6fec4a405fa7790652e305e28ef9131824
Valerie4792 <vaguilar4792@gmail.com>
2023-08-16 19:52:07 -0700

    fixed ternerary operator

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = ({selectedGenre}:Props) => {
   const { data, error, isLoading } = useGames(selectedGenre);
 
   //
-  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16,17,18,19,20];
 
   // need the array dependency to prevent infinite loop
   return (
